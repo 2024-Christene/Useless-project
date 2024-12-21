2ad0dae8516927f4c69b8027e6109020bf8664c7
Meenakshi <meenakshi.u.rajesh@gmail.com>
2024-12-21 09:07:06 +0530

    README.md

--- README.py
+++ README.py
@@ -1,2 +1,9 @@
-Welcome to Useless Project
+# Bus tracking app
+A simple desktop bus tracking application built with Python and Tkinter.
+
+## Features:
+- Real-time updates on bus speed, traffic, and ETA.
+- Bus fee payment simulation.
+- GUI for interaction.
+
 
